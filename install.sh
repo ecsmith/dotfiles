a28af125c955c26426dddff118f4b7b6f2f9bd93
Eric C. Smith <hi@ericsmithux.com>
2017-12-12 17:31:11 -0500

    adding more items to Brewfile

--- install.sh
+++ install.sh
@@ -1,18 +1,28 @@
+#!/bin/bash
+
 which -s brew
 if [[ $? != 0 ]] ; then
     # Install Homebrew
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
-    brew update
+	brew update
 fi
 
-#!/bin/bash
 brew tap Homebrew/bundle
 
 # Homebrew Stufffff
 sh ~/.brew
 brew bundle
 
-ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/sublime
+which -s subl
+if [[ $? != 0 ]] ; then
+    # Create subl soft link
+    ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/sublime
+else
+	echo "subl soft link already created"
+fi
+
+cp .bash_profile ~/.bash_profile
+source ~/.bash_profile
 
 
