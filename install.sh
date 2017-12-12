e32ecffa832dd48771464beb689a1c0dcfbfb1d9
Eric C. Smith <ericsmithux@gmail.com>
2017-12-12 10:47:12 -0500

    check if homebrew installed and if not install it

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    brew update
+fi
+
 #!/bin/bash
 brew tap Homebrew/bundle
 
